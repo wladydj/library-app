c3bdc56ca5f1ebe1f0c1c57e80c0bc2b46d871ee
Vladimir Henriquez <wladydj@gmail.com>
2024-05-17 13:37:15 -0500

    page not found

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../styles/NotFound.css';
+import NotFounds from '../pngwing.com.png'
 
 function NotFound() {
   return (
-    <div>
-        <h1>404 - NotFound</h1>
+    <>
+    <div className="divider"></div>
+    <div className='container-notfound'>
+    <header className='notfound-message'>
+    <h1 className='notfound-message__tittle'>404 - Not Found</h1>
+    <h2 className='notfound-message__description'>El recurso al que estás intentando acceder no fue encontrado.</h2>
+    </header>
+    <div className='container-img-notfound'>
+      <img src={NotFounds} alt='Not found' className='not-found'/>
     </div>
+    </div>
+    </>
   );
 };
 
